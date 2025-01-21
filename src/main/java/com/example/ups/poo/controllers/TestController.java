5c2e8b0dc9aaab15309106b2e474ab9bf87688d3
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2025-01-21 09:18:24 -0500

    Add(I): Add Test Controller with greet and hello controllers

---
+++ src/main/java/com/example/ups/poo/controllers/TestController.java
@@ -0,0 +1,28 @@
+package com.example.ups.poo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "hello World, this is my first SpringBoot Project...!";
+    }
+
+    @GetMapping("/greet/{name}/{lastname}")
+    public String greet(@PathVariable String name, @PathVariable String lastname) {
+        String message = "Hello " + name + " " +
+                lastname + ", this is my first SpringBoot Project...!";
+        return message;
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name, @RequestParam String lastname) {
+        String message = "Hello " + name + " " +
+                lastname + ", this is my first SpringBoot Project xx!";
+        return message;
+    }
+}
