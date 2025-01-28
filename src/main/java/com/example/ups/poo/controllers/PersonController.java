27fc94e5bfd42987078e93fa5cf7abe3d9c6f0d8
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2025-01-28 09:36:23 -0500

    Add(I): Person By Id

--- src/main/java/com/example/ups/poo/controllers/PersonController.java
+++ src/main/java/com/example/ups/poo/controllers/PersonController.java
@@ -2,7 +2,10 @@ package com.example.ups.poo.controllers;
 
 import com.example.ups.poo.dto.Person;
 import com.example.ups.poo.service.PersonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,13 +14,17 @@ import java.util.List;
 public class PersonController {
     private final PersonService personService;
 
-    public PersonController(PersonService personService){
+    public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
     @GetMapping("/get-all-people")
-    public List<Person> getAllPeople(){
-        return personService.getAllPeople();
+    public ResponseEntity getAllPeople() {
+        return ResponseEntity.status(HttpStatus.OK).body(personService.getAllPeople());
+    }
 
+    @GetMapping("/get-person")
+    public ResponseEntity getPersonById(@RequestParam String id){
+        return  personService.getPersonById(id);
     }
 }
--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -1,6 +1,8 @@
 package com.example.ups.poo.service;
 
 import com.example.ups.poo.dto.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,12 +10,29 @@ import java.util.List;
 
 @Service
 public class PersonService {
-    public List<Person> getAllPeople(){
-        List<Person> personList = new ArrayList<>();
-        Person p1 = new Person("Maria", "Barzola", 20, "094380189");
-        Person p2 = new Person("Jorge", "Encalada", 29, "1204510497");
+
+    List<Person> personList = new ArrayList<>();
+
+    public List<Person> getAllPeople() {
+        Person p1 = new Person();
+        p1.setName("Maria");
+        p1.setLastname("Barzola");
+        p1.setAge(20);
+        p1.setId("0943803189");
+
+        Person p2 = new Person("Andrew", "Garfield", 30, "0910452671");
+
         personList.add(p1);
         personList.add(p2);
         return personList;
+    }
+
+    public ResponseEntity getPersonById(String id) {
+        for(Person person: personList) {
+            if(id.equalsIgnoreCase(person.getId())) {
+                return ResponseEntity.status(HttpStatus.OK).body(person);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id; "+id + " not found");
     }
 }
