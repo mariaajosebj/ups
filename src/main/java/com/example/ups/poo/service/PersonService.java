136bb47384370266a8d5abdf25c11a3a7cf8f2e7
mariaajosebj <mbarzolaj@est.ups.edu.ec>
2025-01-29 09:18:29 -0500

    Add(II): Add Create Person

--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -13,26 +13,26 @@ public class PersonService {
 
     List<Person> personList = new ArrayList<>();
 
-    public List<Person> getAllPeople() {
-        Person p1 = new Person();
-        p1.setName("Maria");
-        p1.setLastname("Barzola");
-        p1.setAge(20);
-        p1.setId("0943803189");
-
-        Person p2 = new Person("Andrew", "Garfield", 30, "0910452671");
-
-        personList.add(p1);
-        personList.add(p2);
-        return personList;
+    public ResponseEntity getAllPeople() {
+        if (personList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person List is empty");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(personList);
     }
 
     public ResponseEntity getPersonById(String id) {
-        for(Person person: personList) {
-            if(id.equalsIgnoreCase(person.getId())) {
+        for (Person person : personList) {
+            if (id.equalsIgnoreCase(person.getId())) {
                 return ResponseEntity.status(HttpStatus.OK).body(person);
             }
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id; "+id + " not found");
+        String message = "Person with id: " + id + "not found";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id; "+ id + " not found");
+    }
+
+    public ResponseEntity createPerson(Person person) {
+        personList.add(person);
+        return ResponseEntity.status(HttpStatus.OK).body("Person successfully registered");
     }
 }
+
